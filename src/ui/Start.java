6d62498c2ae608cc4517e6c6ccf0f7e64769be71
ciroH <63865991+ciroH@users.noreply.com>
2021-12-15 16:28:47 -0300

    Added basic files and folder structure

---
+++ src/ui/Start.java
@@ -0,0 +1,12 @@
+package ui;
+
+import javax.swing.JFrame;
+
+public class Start extends JFrame{
+	private static final long serialVersionUID = 1L;
+
+	public static void main(String[] args) {
+		
+	}
+
+}
