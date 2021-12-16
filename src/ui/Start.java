1aa87d10567d632867f84425ebcceba029efea3d
ciroH <ciroH@users.noreply.github.com>
2021-12-16 13:37:09 -0300

    Defined contentPane for the main JFrame

--- src/ui/Start.java
+++ src/ui/Start.java
@@ -1,12 +1,24 @@
 package ui;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import java.awt.Color;
 
 public class Start extends JFrame{
 	private static final long serialVersionUID = 1L;
-
+	private JPanel contentPane;
 	public static void main(String[] args) {
 		
 	}
-
+	
+	public Start() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBackground(Color.GRAY);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+	}
 }
