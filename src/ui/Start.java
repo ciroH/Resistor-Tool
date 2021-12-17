6620d868328978c96ff3d98f5c1bf2482c14a123
ciroH <ciroH@users.noreply.github.com>
2021-12-17 12:12:55 -0300

    Added methods to obtain width and height

--- src/ui/Start.java
+++ src/ui/Start.java
@@ -3,19 +3,25 @@ package ui;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-
+import java.awt.GraphicsEnvironment;
 import java.awt.Color;
 
 public class Start extends JFrame{
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	public static void main(String[] args) {
-		
+		int width = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDisplayMode().getWidth();
+		int height = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDisplayMode().getHeight();
+		System.out.println(width+"x"+height);
+		Start userInterface = new Start();
+		userInterface.setVisible(true);
+		userInterface.setResizable(false);
 	}
 	
 	public Start() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBackground(Color.GRAY);
+		setBounds(0,0,800,600);
 		contentPane = new JPanel();
 		contentPane.setBackground(Color.GRAY);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
