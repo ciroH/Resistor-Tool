0882baf6b72f349dac0e3de5c3fec0403ee980c4
ciroH <ciroH@users.noreply.github.com>
2022-01-17 00:29:52 -0300

    Added measureResistor method to Logic layer

--- src/logic/Processing.java
+++ src/logic/Processing.java
@@ -1,5 +1,11 @@
 package logic;
 
 public class Processing {
-
+	ResistorObject resistorReference = new ResistorObject();
+	
+	public String measureResistor(String numberOfBands, String digit1Color, String digit2Color, String digit3Color, String multiplierColor, String toleranceColor, String tempCoeffColor) {
+		
+			
+	return "";}
+	
 }
