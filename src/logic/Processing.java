c71822305cba4baba7b2da01285e44699f740610
ciroH <ciroH@users.noreply.github.com>
2022-01-19 00:57:36 -0300

    Modified String formatting on measureResistor

--- src/logic/Processing.java
+++ src/logic/Processing.java
@@ -20,26 +20,26 @@ public class Processing {
 		switch (numberOfBands) {
 		case "3 Bands":
 			resistorValue = Integer.parseInt(digit1.toString() + digit2.toString()) * multiplier;
-			results = resistorValue.toString() + "Ω\n" + tolerance;
+			results = "<html><body>" + resistorValue.toString() + "Ω<br>" + tolerance + "</body></html>";
 			break;
 		case "4 Bands":
 			resistorValue = Integer.parseInt(digit1.toString() + digit2.toString()) * multiplier;
-			results = resistorValue.toString() + "Ω\n" + tolerance;
+			results = "<html><body>" + resistorValue.toString() + "Ω<br>" + tolerance + "</body></html>";
 			break;
 		case "5 Bands":
 			digit3 = resistorReference.getDigit3(digit3Color);
 			resistorValue = Integer.parseInt(digit1.toString() + digit2.toString() + digit3.toString()) * multiplier;
-			results = resistorValue.toString() + "Ω\n" + tolerance;
+			results = "<html><body>" + resistorValue.toString() + "Ω<br>" + tolerance + "</body></html>";
 			break;
 		case "6 Bands":
 			digit3 = resistorReference.getDigit3(digit3Color);
 			tempCoeff = resistorReference.getTempCoeff(tempCoeffColor);
 			resistorValue = Integer.parseInt(digit1.toString() + digit2.toString() + digit3.toString()) * multiplier;
-			results = resistorValue.toString() + "Ω\n" + tolerance + "\n"  + tempCoeff.toString() + "ppm";
+			results = "<html><body>" + resistorValue.toString() + "Ω<br>" + tolerance + "<br>"  + tempCoeff.toString() + "ppm" + "</body></html>";
 			break;
 		default:
 			resistorValue = Integer.parseInt(digit1.toString() + digit2.toString()) * multiplier;
-			results = resistorValue.toString() + "Ω\n" + tolerance;
+			results = "<html><body>" + resistorValue.toString() + "Ω<br>" + tolerance + "</body></html>";
 			break;
 		}
 		 
